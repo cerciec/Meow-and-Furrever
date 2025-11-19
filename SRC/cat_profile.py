f8d502c409073194f9a6ec0ee90198d497d4951a
cerciec <ccolem85@students.kennesaw.edu>
2025-11-19 17:31:35 -0500

    Add common and exotic cat breeds lists
    Added lists of common and exotic cat breeds.

--- SRC/cat_profile.py
+++ SRC/cat_profile.py
@@ -1,1 +1,4 @@
-
+# cat_profile.py
+# common cat breeds
+COMMON_BREEDS = ["Domestic Shorthair", "American Shorthair", "Domestic Longhair", "Siamese", "Ragdoll", "Maine Coon", "Other / Mixed / Unknown"]
+EXOTIC_BREEDS = ["Abyssinian", "Bengal", "Bombay", "British Shorthair", "Devon Rex", "Exotic Shorthair", "Persian", "Russian Blue", "Scottish Fold", "Siberian", "Sphynx", "Other / Mixed / Unknown"
